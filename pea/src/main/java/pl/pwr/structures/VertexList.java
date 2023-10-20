880a37869be060e2ba9ec9dcbf511a625837c63e
durki <michal@durkalec.com>
2023-10-20 15:34:50 +0200

    Created Decorator fot ArrayList

---
+++ pea/src/main/java/pl/pwr/structures/VertexList.java
@@ -0,0 +1,17 @@
+package pl.pwr.structures;
+
+import java.util.ArrayList;
+
+// This class is a decorator for ArrayList<Integer> and adds the functionality of generating permutations of a list. 
+public class VertexList extends ArrayList<Integer> {
+
+    // Generates next mathematical permutation of elements in ArrayList
+    public void nextPermutation() {
+
+    }
+
+    // Counts all possible permutations of elements in ArrayList
+    public int countPermutations() {
+        return 0;
+    }
+}
