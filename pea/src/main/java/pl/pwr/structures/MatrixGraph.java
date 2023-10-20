12b26fedf07ddde77252d8249e6eef0da94ae015
durki <michal@durkalec.com>
2023-10-20 15:19:31 +0200

    Edited gitignore

--- pea/src/main/java/pl/pwr/structures/MatrixGraph.java
+++ pea/src/main/java/pl/pwr/structures/MatrixGraph.java
@@ -1,9 +1,19 @@
 package pl.pwr.structures;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 /**
  * MatrixGraph
  */
 public class MatrixGraph {
+
+    private ArrayList<ArrayList<Optional<Integer>>> matrix;
+
+    public MatrixGraph(ArrayList<ArrayList<Optional<Integer>>> matrix) {
+        this.matrix = matrix;
+    }
+
     public static void test() {
         System.out.println("Hello World!");
     }
