736ce24a415b1d999884b6e6129dc16f67ee113f
durki <michal@durkalec.com>
2023-11-14 00:42:15 +0100

    Working DP Recursive approach

--- pea/src/main/java/pl/pwr/measure/MeasureSuite.java
+++ pea/src/main/java/pl/pwr/measure/MeasureSuite.java
@@ -5,8 +5,6 @@ import pl.pwr.algorithms.TSPAlgorithmType;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
--- pea/src/main/java/pl/pwr/measure/MeasureTask.java
+++ pea/src/main/java/pl/pwr/measure/MeasureTask.java
@@ -3,9 +3,6 @@ package pl.pwr.measure;
 import pl.pwr.algorithms.TSPAlgorithmType;
 import pl.pwr.structures.FullGraphFactory;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadMXBean;
-
 import java.util.concurrent.Callable;
 
 public class MeasureTask implements Callable<Long> {
@@ -20,18 +17,11 @@ public class MeasureTask implements Callable<Long> {
 
     @Override
     public Long call() {
-        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
         var instance = FullGraphFactory.generateRandom(vertexCount);
         var algorithm = TSPAlgorithmType.getAlgorithm(algorithmType, instance);
-//        long startTime = getThreadTime(bean);
         long startTime = System.nanoTime();
         algorithm.solve();
         long endTime = System.nanoTime();
-//        long endTime = getThreadTime(bean);
         return endTime - startTime;
-    }
-
-    private static long getThreadTime(ThreadMXBean bean) {
-        return bean.getCurrentThreadUserTime();
     }
 }
